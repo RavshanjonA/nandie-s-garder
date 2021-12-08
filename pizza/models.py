1fa4681c9b68a6d868d66004c9a24cf836fdba09
Ahmadjonov-98 <akhamdjonovr.98@gmail.com>
2021-12-08 21:01:15 +0500

    create size and pizza model for pizza app

--- pizza/models.py
+++ pizza/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Size(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Pizza(models.Model):
+    topping1 = models.CharField(max_length=64)
+    topping2 = models.CharField(max_length=64)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
--- pizza/views.py
+++ pizza/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from pizza.forms import PizzaForm
 
@@ -8,5 +9,18 @@ def home(request):
 
 
 def order(request):
-    form = PizzaForm()
-    return render(request, 'pizza/order.html', context={"form": form})
+    if request.method == "POST":
+        form = PizzaForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            note = "Thanks for ordering, your {0} {1} and {2} pizza on the way".format(data["size"], data["topping1"],
+                                                                                       data["topping2"])
+            # messages.success(request,"Your order was successfully added")
+            form = PizzaForm()
+            return render(request, "pizza/order.html", context={"form": form, "note": note})
+        else:
+            messages.error(request, "Error saving form")
+        return redirect("home")
+    else:
+        form = PizzaForm()
+        return render(request, 'pizza/order.html', context={"form": form})
