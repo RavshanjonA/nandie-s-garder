8b7a882c90dfd9cc9469073833470e941d78213e
Ravshan <akhamadjonovr.98@gamil.com>
2021-12-21 21:54:37 +0500

    add silenium for home testing

--- pizza/tests.py
+++ pizza/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from selenium import webdriver
 
-# Create your tests here.
+class FunctionalTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def test_there_is_home_page(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('install', self.browser.page_source)
+
+    def testDown(self):
+        self.browser.quit()
